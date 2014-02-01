e57b9e4ec3f77bf5fce6f9d60456dc49e5b9665a
Mark McGuire <mark.b.mcg@gmail.com>
2014-02-01 17:13:22 -0600

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -e /srv/salt ]
+then
+  rm -rf /srv/salt
+fi
 cp -r state /srv/salt
+
+if [ -e /srv/pillar ]
+then
+  rm -rf /srv/pillar
+fi
 cp -r pillar /srv/pillar
-cp minion /etc/salt/minion
