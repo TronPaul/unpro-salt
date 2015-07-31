cd1cbcf61202c733d5c5b65ad35775d3ad40ab74
Mark McGuire <mmcguire@backstopsolutions.com>
2015-07-31 15:47:01 -0500

    fix logdir and salt verbosity

--- salt/pre-bootstrap/ubuntu/trusty64.sh
+++ salt/pre-bootstrap/ubuntu/trusty64.sh
@@ -4,3 +4,4 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" python-pip python-dev
 sudo pip install -U 'tornado >= 4.0'
+sudo mkdir /var/log/salt
